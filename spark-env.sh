3251d501899af5d1a8a385dd5a1726f7e685ad62
jinyu <jinyu.zyp@alibaba-inc.com>
2019-09-20 11:05:43 +0800

    spark final

--- spark-env.sh
+++ spark-env.sh
@@ -67,6 +67,6 @@
 # You might get better performance to enable these options if using native BLAS (see SPARK-21305).
 # - MKL_NUM_THREADS=1        Disable multi-threading of Intel MKL
 # - OPENBLAS_NUM_THREADS=1   Disable multi-threading of OpenBLAS
-export HADOOP_CONF_DIR=/home/feiwang/asplos/hadoop
+export HADOOP_CONF_DIR=/home/feiwang/asplos/hadoop/etc/hadoop
 export SPARK_HOME=/home/feiwang/asplos/spark
 export SPARK_DAEMON_MEMORY=4g
